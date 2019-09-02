57d531a3af17b2f2b125ba8d24b44c00ad82e40b
rajivgandhi2552 <54628472+rajivgandhi2552@users.noreply.github.com>
2019-09-02 16:42:48 +0530

    Add files via upload
    Added TestSuit for all test cases

---
+++ src/hexad/bakery/test/AllTests.java
@@ -0,0 +1,11 @@
+package hexad.bakery.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestCFProduct.class, TestMB11Product.class, TestVS5Product.class })
+public class AllTests {
+
+}
