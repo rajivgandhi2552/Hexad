59515ff1585a4fa12be40821e9cb511fa1396dfd
rajivgandhi2552 <54628472+rajivgandhi2552@users.noreply.github.com>
2019-09-02 09:44:33 +0530

    Add files via upload
    Added new product catalog class

---
+++ src/hexad/retail/bakery/product/ProductOrdering.java
@@ -0,0 +1,17 @@
+package hexad.retail.bakery.product;
+
+import java.util.List;
+
+public class ProductOrdering {
+
+	public void loadProductCatalog() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<SubPoductCart> selectBestPacks(String string, int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
